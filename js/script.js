0d509503f53803849597789469173a8ea7725865
Saves <saves1484@gmail.com>
2022-01-21 11:58:54 -0800

    array of all characters/unfinished mixedChar function

--- js/script.js
+++ js/script.js
@@ -7,5 +7,15 @@ const lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
 const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 const numbers = '0123456789'.split('');
 const symbols = '`~!@#$%^&*()-_=+[{]};:",<.>/?'.split('');
+//array of character arrays
+const characters = [];
+characters.push(lowerCase, upperCase, numbers, symbols)
 //function that outputs an array of random characters
+function mixedCharacters(num) {
+    let charArray = [];
+    let number = randomNumber();
+    for (i = 0; i < num; i++) {
+
+    }
+}
 
