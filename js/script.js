a98e51fba63ba4e80ff60bdc8a047be32c7dfcf5
Saves <saves1484@gmail.com>
2022-01-21 11:31:57 -0800

    character arrays

--- js/script.js
+++ js/script.js
@@ -3,5 +3,9 @@ function randomNumber(num) {
     return Math.floor(Math.random() * num)
 }
 //4 arrays (one for each character type)
-
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const numbers = '0123456789'.split('');
+const symbols = '`~!@#$%^&*()-_=+[{]};:",<.>/?'.split('');
 //function that outputs an array of random characters
+
