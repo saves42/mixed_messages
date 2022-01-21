97d7c80ae0eb9b7a308a4260c570c0806cd5616a
Saves <saves1484@gmail.com>
2022-01-21 11:07:35 -0800

    random number function

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//random number generator
+function randomNumber(num) {
+    return Math.floor(Math.random() * num)
+}
+//4 arrays (one for each character type)
+
+//function that outputs an array of random characters
